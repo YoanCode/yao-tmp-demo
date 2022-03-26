1b220e3703a297512c5f8a813900ada2810de52b
yoan <yeoan11317@gmail.com>
2022-03-26 18:05:55 +0800

    建立 框架Pakage架構

---
+++ src/main/java/yao/tmp/demo/YaoTmpDemoApplication.java
@@ -0,0 +1,13 @@
+package yao.tmp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaoTmpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaoTmpDemoApplication.class, args);
+	}
+
+}
